d9f3be3a42d0ef3f9da3ed8109dcf175ed38c31d
Lucas Lee <leelucas12996@gmail.com>
2024-03-13 18:28:39 +0900

    fix: conflict resolve

--- src/pages/Safari/index.tsx
+++ src/pages/Safari/index.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Slot from '../../component/Slots';
+import get_winning_paylines from '../../utils/get_winning_paylines';
 
 import Background from '../../assets/background.png';
 import MinusImage from '../../assets/minus.png';
@@ -45,7 +46,11 @@ const Safari = () => {
   const [sideLeft, setSideLeft] = useState(SideLeft1);
   const [sideRight, setSideRight] = useState(SideRight1);
   // const [isSpinClicked, setIsSpinClicked] = useState(false);
-
+  const [result1, setResult1] = useState<String[]>(() => []);
+  const [result2, setResult2] = useState<String[]>(() => []);
+  const [result3, setResult3] = useState<String[]>(() => []);
+  const [result4, setResult4] = useState<String[]>(() => []);
+  const [result5, setResult5] = useState<String[]>(() => []);
   useEffect(() => {
     switch (line) {
       case 1:
@@ -138,7 +143,8 @@ const Safari = () => {
   };
 
   return (
-    <div className="flex flex-col 2xl:w-[1612px] h-[100vh] xl:w-[1200px] lg:w-[960px] md:w-[700px] sm:w-[540px] bg-no-repeat inset-0 2xl:h-[906px] xl:h-[576px] lg:h-[462px] md:h-[335px] sm:h-[257px] bg-cover rotate"
+    <div
+      className="flex flex-col 2xl:w-[1612px] h-[100vh] xl:w-[1200px] lg:w-[960px] md:w-[700px] sm:w-[540px] bg-no-repeat inset-0 2xl:h-[906px] xl:h-[576px] lg:h-[462px] md:h-[335px] sm:h-[257px] bg-cover rotate"
       style={{
         backgroundImage: `url(${Background})`,
         backgroundSize: 'cover',
@@ -147,176 +153,197 @@ const Safari = () => {
       {/* <div
         className=" "
       > */}
-        <div className="flex justify-between h-[50px]">
-          <div className="flex mt-[4px]">
-            <p className="text-white text-center font-extrabold text-[24px] w-[493px] pl-[275px]">
-              864.30
-            </p>
-            <p className="text-white font-extrabold text-center text-[24px] pl-[157px]  w-[450px]">
-              5864.30
-            </p>
-            <p className="text-white font-extrabold text-[24px] text-center pl-[150px] w-[365px]">
-              864.30
-            </p>
-          </div>
-          <div>
-            <button
+      <div className="flex justify-between h-[50px]">
+        <div className="flex mt-[4px]">
+          <p className="text-white text-center font-extrabold text-[24px] w-[493px] pl-[275px]">
+            864.30
+          </p>
+          <p className="text-white font-extrabold text-center text-[24px] pl-[157px]  w-[450px]">
+            5864.30
+          </p>
+          <p className="text-white font-extrabold text-[24px] text-center pl-[150px] w-[365px]">
+            864.30
+          </p>
+        </div>
+        <div>
+          <button
+            onClick={toggleDrawer}
+            className="w-[71px] h-[91px] focus:outline-none hover:brightness-125 bg-no-repeat bg-center border-none p-0"
+            style={{ backgroundImage: `url(${MenuBtnImage})` }}
+          ></button>
+          {isOpen && (
+            <div
+              className="fixed inset-0 z-20 bg-black bg-opacity-50"
               onClick={toggleDrawer}
-              className="w-[71px] h-[91px] focus:outline-none hover:brightness-125 bg-no-repeat bg-center border-none p-0"
-              style={{ backgroundImage: `url(${MenuBtnImage})` }}
-            ></button>
-            {isOpen && (
-              <div
-                className="fixed inset-0 z-20 bg-black bg-opacity-50"
-                onClick={toggleDrawer}
-              ></div>
-            )}
-            <div
-              className={`fixed top-0 z-40 w-[326px] h-[906px] shadow-lg transform ease-in-out duration-300 ${
-                isOpen
-                  ? 'translate-x-0 right-[153px]'
-                  : 'translate-x-full right-[180px] hidden'
-              }`}
-              style={{ backgroundImage: `url(${MenuImage})` }}
-            >
-              {/* Drawer content here */}
-              <div className="flex flex-col gap-[37.5px] pt-[162px] px-[21px] ">
-                <button
-                  onClick={() => navigate('/')}
-                  className="h-[83px] w-[286px] focus:outline-none hover:brightness-110 bg-no-repeat bg-center border-none"
-                  style={{ backgroundImage: `url(${MenuBackImage})` }}
-                ></button>
-                <button
-                  onClick={() => navigate('/safari/help')}
-                  className="h-[83px] w-[286px] focus:outline-none hover:brightness-110 bg-no-repeat bg-center border-none"
-                  style={{ backgroundImage: `url(${MenuHelpImage})` }}
-                ></button>
-                <button
-                  className="h-[83px] w-[286px] focus:outline-none hover:brightness-110 bg-no-repeat bg-center border-none"
-                  style={{ backgroundImage: `url(${MenuAudioOffImage})` }}
-                ></button>
-                <button
-                  className="h-[83px] w-[286px] focus:outline-none hover:brightness-110 bg-no-repeat bg-center border-none"
-                  style={{ backgroundImage: `url(${MenuShakeImage})` }}
-                ></button>
-                <button
-                  className="h-[83px] w-[286px] focus:outline-none hover:brightness-110 bg-no-repeat bg-center border-none"
-                  style={{ backgroundImage: `url(${MenuLogoutImage})` }}
-                ></button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex pt-[102px] gap-[19.2px]">
-          <div className="flex gap-[3px]">
-            <div
-              className="w-[64px] h-[628px] mt-[-11px]"
-              style={{
-                backgroundImage: `url(${sideLeft})`,
-              }}
             ></div>
-            <Slot count={9} isSpinning={isSpinning} onSpinEnd={handleSpinEnd} />
-          </div>
-          <Slot count={12} isSpinning={isSpinning} onSpinEnd={handleSpinEnd} />
-          <Slot count={15} isSpinning={isSpinning} onSpinEnd={handleSpinEnd} />
-          <Slot count={18} isSpinning={isSpinning} onSpinEnd={handleSpinEnd} />
-          <div className="flex gap-[4px]">
-            <Slot
-              count={21}
-              isSpinning={isSpinning}
-              onSpinEnd={handleSpinEnd}
-            />
-            <div
-              className="w-[64px] h-[628px] mt-[-12px]"
-              style={{
-                backgroundImage: `url(${sideRight})`,
-              }}
-            ></div>
-          </div>
-        </div>
-        <div className="flex mt-[2px]">
-          <div className="w-[374px]">
-            <p className="gradient-text pt-[55px] bg-[#300E0C] text-[36px] font-bold">
-              182354324
-            </p>
-          </div>
-          <div className="w-[373px]">
-            <p className="gradient-text  text-[36px] font-bold pl-[76px] mt-[-4px]">
-              {line}
-            </p>
-            <div className="flex mt-[2px]">
+          )}
+          <div
+            className={`fixed top-0 z-40 w-[326px] h-[906px] shadow-lg transform ease-in-out duration-300 ${
+              isOpen
+                ? 'translate-x-0 right-[153px]'
+                : 'translate-x-full right-[180px] hidden'
+            }`}
+            style={{ backgroundImage: `url(${MenuImage})` }}
+          >
+            {/* Drawer content here */}
+            <div className="flex flex-col gap-[37.5px] pt-[162px] px-[21px] ">
               <button
-                type="submit"
-                onClick={handleDecrementLine}
-                className="h-[81px] w-[173px] focus:outline-none hover:brightness-125 bg-no-repeat bg-center border-none"
-                style={{
-                  backgroundImage: isSpinning ? '' : `url(${MinusImage})`,
-                }}
+                onClick={() => navigate('/')}
+                className="h-[83px] w-[286px] focus:outline-none hover:brightness-110 bg-no-repeat bg-center border-none"
+                style={{ backgroundImage: `url(${MenuBackImage})` }}
               ></button>
               <button
-                type="submit"
-                onClick={handleIncrementLine}
-                className="h-[81px] w-[172px] focus:outline-none hover:brightness-125 bg-no-repeat bg-center border-none ml-[-2px]"
-                style={{
-                  backgroundImage: isSpinning ? '' : `url(${PlusImage})`,
-                }}
-              ></button>
-            </div>
-          </div>
-          <div className="w-[373px]">
-            <p className="gradient-text text-[36px] font-bold pl-[40px] mt-[-4px]">
-              {(betValue * line).toFixed(2)}
-            </p>
-            <div className="flex mt-[2px] ml-[7px]">
-              <button
-                type="submit"
-                onClick={handleDecrementBet}
-                className="h-[81px] w-[172px] focus:outline-none hover:brightness-125 bg-no-repeat bg-center border-none"
-                style={{
-                  backgroundImage: isSpinning ? '' : `url(${MinusImage})`,
-                }}
+                onClick={() => navigate('/safari/help')}
+                className="h-[83px] w-[286px] focus:outline-none hover:brightness-110 bg-no-repeat bg-center border-none"
+                style={{ backgroundImage: `url(${MenuHelpImage})` }}
               ></button>
               <button
-                type="submit"
-                // onMouseEnter={handleIncrementBet}
-                onClick={handleIncrementBet}
-                className="h-[81px] w-[172px] focus:outline-none hover:brightness-125 bg-no-repeat bg-center border-none ml-[-2px]"
-                style={{
-                  backgroundImage: isSpinning ? '' : `url(${PlusImage})`,
-                }}
-              ></button>
-            </div>
-          </div>
-          <div className="w-auto">
-            <p className="gradient-text text-[36px] font-bold pl-[70px] mt-[-4px]">
-              {(betValue * line).toFixed(2)}
-            </p>
-            <div className="flex gap-[6px] mt-[2px] ml-[7px]">
-              <button
-                type="submit"
-                // onClick={handleDecrementLine}
-                onClick={handleSpinClick}
-                className="h-[81px] w-[243px] focus:outline-none hover:brightness-125 bg-no-repeat bg-center border-none"
-                style={{
-                  backgroundImage: isSpinning
-                    ? `url(${AutoStopImage})`
-                    : `url(${AutoStartImage})`,
-                }}
+                className="h-[83px] w-[286px] focus:outline-none hover:brightness-110 bg-no-repeat bg-center border-none"
+                style={{ backgroundImage: `url(${MenuAudioOffImage})` }}
               ></button>
               <button
-                type="submit"
-                onClick={handleSpinClick}
-                className="focus:outline-none hover:brightness-125 border-none w-[234px] h-[79px] bg-opacity-0 aspect-auto object-cover"
-                style={{
-                  backgroundImage: isSpinning
-                    ? `url(${StopSpinImage})`
-                    : `url(${SpinImage})`,
-                }}
+                className="h-[83px] w-[286px] focus:outline-none hover:brightness-110 bg-no-repeat bg-center border-none"
+                style={{ backgroundImage: `url(${MenuShakeImage})` }}
+              ></button>
+              <button
+                className="h-[83px] w-[286px] focus:outline-none hover:brightness-110 bg-no-repeat bg-center border-none"
+                style={{ backgroundImage: `url(${MenuLogoutImage})` }}
               ></button>
             </div>
           </div>
         </div>
       </div>
+      <div className="flex pt-[102px] gap-[19.2px]">
+        <div className="flex gap-[3px]">
+          <div
+            className="w-[64px] h-[628px] mt-[-11px]"
+            style={{
+              backgroundImage: `url(${sideLeft})`,
+            }}
+          ></div>
+          <Slot
+            count={9}
+            isSpinning={isSpinning}
+            setResult={setResult1}
+            onSpinEnd={handleSpinEnd}
+          />
+        </div>
+        <Slot
+          count={12}
+          isSpinning={isSpinning}
+          setResult={setResult2}
+          onSpinEnd={handleSpinEnd}
+        />
+        <Slot
+          count={15}
+          isSpinning={isSpinning}
+          setResult={setResult3}
+          onSpinEnd={handleSpinEnd}
+        />
+        <Slot
+          count={18}
+          isSpinning={isSpinning}
+          setResult={setResult4}
+          onSpinEnd={handleSpinEnd}
+        />
+        <div className="flex gap-[4px]">
+          <Slot
+            count={21}
+            isSpinning={isSpinning}
+            setResult={setResult5}
+            onSpinEnd={handleSpinEnd}
+          />
+          <div
+            className="w-[64px] h-[628px] mt-[-12px]"
+            style={{
+              backgroundImage: `url(${sideRight})`,
+            }}
+          ></div>
+        </div>
+      </div>
+      <div className="flex mt-[2px]">
+        <div className="w-[374px]">
+          <p className="gradient-text pt-[55px] bg-[#300E0C] text-[36px] font-bold">
+            182354324
+          </p>
+        </div>
+        <div className="w-[373px]">
+          <p className="gradient-text  text-[36px] font-bold pl-[76px] mt-[-4px]">
+            {line}
+          </p>
+          <div className="flex mt-[2px]">
+            <button
+              type="submit"
+              onClick={handleDecrementLine}
+              className="h-[81px] w-[173px] focus:outline-none hover:brightness-125 bg-no-repeat bg-center border-none"
+              style={{
+                backgroundImage: isSpinning ? '' : `url(${MinusImage})`,
+              }}
+            ></button>
+            <button
+              type="submit"
+              onClick={handleIncrementLine}
+              className="h-[81px] w-[172px] focus:outline-none hover:brightness-125 bg-no-repeat bg-center border-none ml-[-2px]"
+              style={{
+                backgroundImage: isSpinning ? '' : `url(${PlusImage})`,
+              }}
+            ></button>
+          </div>
+        </div>
+        <div className="w-[373px]">
+          <p className="gradient-text text-[36px] font-bold pl-[40px] mt-[-4px]">
+            {(betValue * line).toFixed(2)}
+          </p>
+          <div className="flex mt-[2px] ml-[7px]">
+            <button
+              type="submit"
+              onClick={handleDecrementBet}
+              className="h-[81px] w-[172px] focus:outline-none hover:brightness-125 bg-no-repeat bg-center border-none"
+              style={{
+                backgroundImage: isSpinning ? '' : `url(${MinusImage})`,
+              }}
+            ></button>
+            <button
+              type="submit"
+              // onMouseEnter={handleIncrementBet}
+              onClick={handleIncrementBet}
+              className="h-[81px] w-[172px] focus:outline-none hover:brightness-125 bg-no-repeat bg-center border-none ml-[-2px]"
+              style={{
+                backgroundImage: isSpinning ? '' : `url(${PlusImage})`,
+              }}
+            ></button>
+          </div>
+        </div>
+        <div className="w-auto">
+          <p className="gradient-text text-[36px] font-bold pl-[70px] mt-[-4px]">
+            {(betValue * line).toFixed(2)}
+          </p>
+          <div className="flex gap-[6px] mt-[2px] ml-[7px]">
+            <button
+              type="submit"
+              // onClick={handleDecrementLine}
+              onClick={handleSpinClick}
+              className="h-[81px] w-[243px] focus:outline-none hover:brightness-125 bg-no-repeat bg-center border-none"
+              style={{
+                backgroundImage: isSpinning
+                  ? `url(${AutoStopImage})`
+                  : `url(${AutoStartImage})`,
+              }}
+            ></button>
+            <button
+              type="submit"
+              onClick={handleSpinClick}
+              className="focus:outline-none hover:brightness-125 border-none w-[234px] h-[79px] bg-opacity-0 aspect-auto object-cover"
+              style={{
+                backgroundImage: isSpinning
+                  ? `url(${StopSpinImage})`
+                  : `url(${SpinImage})`,
+              }}
+            ></button>
+          </div>
+        </div>
+      </div>
+    </div>
     // </div>
   );
 };
